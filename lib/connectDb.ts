4425358716e33c84eaf5cbf79c93def5794b6b01
SwapniL1563 <choratswapnil15@gmail.com>
2025-08-25 17:45:50 +0530

    feat:add mongo_uri"

---
+++ lib/connectDb.ts
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI as string;
+
+if (!MONGODB_URI) {
+  throw new Error("⚠️ Please add MONGODB_URI to .env.local");
+}
+
+let isConnected = false;
+
+export async function connectDB() {
+  if (isConnected) return;
+
+  try {
+    await mongoose.connect(MONGODB_URI);
+    isConnected = true;
+    console.log("✅ MongoDB Connected");
+  } catch (err) {
+    console.error("❌ MongoDB connection error:", err);
+    throw err;
+  }
+}
